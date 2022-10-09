7489093679d7707b985ffa1e4fac11b1fce3a9c2
YooInseo <inseo0121@gmail.com>
2022-10-10 02:18:41 +0900

    GUI API 추가

---
+++ src/main/java/skyexcel/gui/GUI.java
@@ -0,0 +1,153 @@
+package skyexcel.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class GUI {
+    private static List<Node> node = new ArrayList<>();
+
+    private Inventory inv;
+
+
+    public GUI(Plugin plugin) {
+        plugin.getServer().getPluginManager().registerEvents(new Event(), plugin);
+    }
+
+    public void createGUI(String title, int size) {
+        inv = Bukkit.createInventory(null, size, title);
+    }
+
+    public void line(ItemStack item, int slot, Consumer<Action> action) {
+        Node node = new Node(slot);
+
+        node.setAction(action);
+
+        this.node.add(node);
+
+        this.inv.setItem(slot, item);
+    }
+
+
+    /***
+     * open 메소드를 사용하기 전에 createGUI 메소드를 무조건
+     * 호출 해야합니다.
+     * @param player 오픈할 플레이어
+     */
+    public void open(Player player) {
+        assert inv != null;
+        player.openInventory(inv);
+    }
+
+    public Inventory getInv() {
+        return inv;
+    }
+
+    private static class Node {
+
+        private int slot;
+
+        private Consumer<Action> action;
+
+        private Node(int slot) {
+            this.slot = slot;
+        }
+
+        public void setAction(Consumer<Action> action) {
+
+            this.action = action;
+        }
+
+        public int getSlot() {
+            return slot;
+        }
+
+        public Consumer<Action> getAction() {
+            return action;
+        }
+    }
+
+    private static class Action {
+        private Player player;
+        private ItemStack item;
+        private int slot;
+        private String title;
+        private Inventory inv;
+
+
+        public Action(Player player, ItemStack item, int slot, String title, Inventory inv) {
+            this.player = player;
+            this.item = item;
+            this.slot = slot;
+            this.title = title;
+            this.inv = inv;
+        }
+
+        public Action(Player player, int slot, String title, Inventory inv) {
+            this.player = player;
+            this.slot = slot;
+            this.title = title;
+            this.inv = inv;
+        }
+
+        public int getSlot() {
+            return slot;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Player getPlayer() {
+            return player;
+        }
+
+        public ItemStack getItem() {
+            return item;
+        }
+
+        public Inventory getInv() {
+            return inv;
+        }
+    }
+
+    private static class Event implements Listener {
+
+        @EventHandler
+        public void onClick(InventoryClickEvent event) {
+            if (event.getWhoClicked() instanceof Player) {
+                Player player = (Player) event.getWhoClicked();
+                int slot = event.getSlot();
+                String title = event.getView().getTitle();
+
+                Inventory inv = event.getClickedInventory();
+                ItemStack item = event.getCurrentItem();
+                if (node != null) {
+                    for (Node newnode : node) {
+                        if (slot == newnode.getSlot()) {
+                            if (item != null) {
+                                Action action = new Action(player, item, slot, title, inv);
+                                newnode.getAction().accept(action);
+
+                            } else {
+                                Action action = new Action(player, slot, title, inv);
+                                newnode.getAction().accept(action);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
