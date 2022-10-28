49c8aaa733d7c5b91a8361c434f65874e9d58d4d
YooInseo <inseo0121@gmail.com>
2022-10-28 19:00:42 +0900

    test

--- src/main/java/skyexcel/data/file/GUI.java
+++ src/main/java/skyexcel/data/file/GUI.java
@@ -2,6 +2,7 @@ package skyexcel.data.file;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.HumanEntity;
@@ -15,10 +16,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+@SuppressWarnings("all")
 public class GUI implements DefaultConfig {
 
     private ConfigurationSection section;
-    private Config yaml;
+    private final Config yaml;
 
     public GUI(Config yaml) {
         this.yaml = yaml;
@@ -34,15 +36,15 @@ public class GUI implements DefaultConfig {
         for (HumanEntity viewers : inv.getViewers()) {
             InventoryView OpenInv = viewers.getOpenInventory();
             if (OpenInv.getTopInventory().equals(inv) && !inv.getType().equals(InventoryType.CRAFTING)) {
+                yaml.setString(path + ".inv.title", OpenInv.getTitle());
+                yaml.setInteger(path + ".inv.size", inv.getSize());
+                yaml.getConfig().createSection(path + ".inv.items");
                 for (int i = 0; i < inv.getSize(); i++) {
                     ItemStack item = inv.getItem(i);
-
-                    yaml.setString(path + ".inv.title", OpenInv.getTitle());
-                    yaml.setInteger(path + ".inv.size", inv.getSize());
+                    if (item != null)
+                        System.out.println(item.getType().name());
                     if (item != null) {
                         setItemStack(path, item, i, i);
-                    } else {
-                        yaml.getConfig().set(path + ".inv.items." + i, null);
                     }
                 }
             }
@@ -67,21 +69,8 @@ public class GUI implements DefaultConfig {
 
     public void setItemStack(String path, ItemStack item, int i, int index) {
         section = yaml.getConfig().createSection(path + ".inv.items." + index);
-        setItemStack(path, item);
-
         section.set("slot", i);
-//        section.set("Material", item.getType().name());
-//        section.set("Amount", item.getAmount());
-//        section.set("Durability", item.getDurability());
-//
-//        ItemMeta meta = item.getItemMeta();
-//        if (item.hasItemMeta()) {
-//            section.set("Meta.display-name", meta.getDisplayName());
-//            section.set("Meta.lore", meta.getLore());
-//
-//        }
-//        section.set("data", item.getDurability());
-
+        setItemStack(path, item);
     }
 
 
@@ -95,72 +84,80 @@ public class GUI implements DefaultConfig {
     @Override
     public void setBoolean(String path, boolean value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setBoolean, section=?");
-        section.set(path, value);
+        section.set(path + ".data", value);
         yaml.saveConfig();
     }
 
     @Override
     public void setString(String path, String value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setString, section=?");
-        section.set(path, value);
+        section.set(path + ".data", value);
         yaml.saveConfig();
     }
 
     @Override
     public void setDouble(String path, double value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setDouble, section=?");
-        section.set(path, value);
+        section.set(path + ".data", value);
         yaml.saveConfig();
     }
 
     public void setItemStack(String path, ItemStack value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setItemStack, section=?");
 
-        if (value.hasItemMeta()) {
-            ItemMeta meta = value.getItemMeta();
-            assert meta != null;
-            if (meta.hasDisplayName()) {
-                section.set(path + ".Display", meta.getDisplayName());
-            }
-            if (meta.hasCustomModelData()) {
-                section.set(path + ".CustomModelData", meta.getCustomModelData());
-            }
-            if (meta.hasEnchants()) {
-                Map<Enchantment, Integer> enchants = meta.getEnchants();
-                for (Enchantment enchantment : enchants.keySet()) {
-                    int level = enchants.get(enchantment);
-                    section.set("Enchant." + enchantment.getName(), level);
-
-                }
-            }
-        }
-
         section.set("Material", value.getType().name());
         section.set("Amount", value.getAmount());
         section.set("Durability", value.getDurability());
 
-        yaml.saveConfig();
-    }
+        if (value.hasItemMeta()) {
 
+            ItemMeta meta = value.getItemMeta();
+            assert meta != null;
+            if (meta.hasDisplayName())
+                section.set("Meta.Display", meta.getDisplayName());
+
+            if (meta.hasCustomModelData())
+                section.set("Meta.CustomModelData", meta.getCustomModelData());
+
+            Map<Enchantment, Integer> enchants = meta.getEnchants();
+
+            for (Enchantment enchantment : value.getEnchantments().keySet()) {
+                int level = enchants.get(enchantment);
+                section.set("Enchant." + enchantment.getName(), level);
+            }
+        }
+        yaml.saveConfig();
+
+        section = section.getConfigurationSection("Enchant");
+
+        if (section != null) {
+            for (String key : section.getKeys(false)) {
+                Enchantment enchantment = Enchantment.getByName("");
+
+
+                System.out.println(key + " : " + section.getInt(key));
+            }
+        }
+    }
 
     @Override
     public void setLong(String path, long value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setLong, section=?");
-        section.set(path, value);
+        section.set(path + ".data", value);
         yaml.saveConfig();
     }
 
     @Override
     public void setList(String path, List<Object> value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setList, section=?");
-        section.set(path, value);
+        section.set(path + ".data", value);
         yaml.saveConfig();
     }
 
     @Override
     public void setObject(String path, Object value) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #setObject, section=?");
-        section.set(path, value);
+        section.set(path + ".data", value);
         yaml.saveConfig();
     }
 
@@ -168,68 +165,81 @@ public class GUI implements DefaultConfig {
     public int getInteger(String path) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getInteger, section=?");
 
-        return section.getInt(path);
+        return section.getInt(path + ".data");
     }
 
     @Override
     public boolean getBoolean(String path) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getBoolean, section=?");
-        return section.getBoolean(path);
+        return section.getBoolean(path + ".data");
     }
 
     @Override
     public String getString(String path) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getString, section=?");
-        return section.getString(path);
+        return section.getString(path + ".data");
     }
 
     @Override
     public double getDouble(String path) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getDouble, section=?");
-        return section.getDouble(path);
+        return section.getDouble(path + ".data");
     }
 
     @Override
     public long getLong(String path) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getLong, section=?");
-        return section.getLong(path);
+        return section.getLong(path + ".data");
     }
 
     @Override
     public Object getObject(String path) {
         Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getObject, section=?");
-        return section.get(path);
+        return section.get(path + ".data");
+    }
+
+
+    public short getShort(String path) {
+        Objects.requireNonNull(section, "section 변수가 없습니다! 확인해 주세요! 에러가 발생된 메소드 : #getObject, section=?");
+        return (short) section.get(path + ".data");
     }
 
     public Inventory getInventory(String path) {
+
         if (yaml.getConfig().get(path + ".inv.size") != null && yaml.getConfig().get(path + ".inv.title") != null) {
-            Inventory inv = Bukkit.createInventory(null, getInteger(path + ".inv.size"), getString(path + ".inv.title"));
+
+
+            Inventory inv = Bukkit.createInventory(null, yaml.getInteger(path + ".inv.size"), yaml.getString(path + ".inv.title"));
+
+            section = yaml.getConfig().getConfigurationSection(path + ".inv.items");
+
             for (int i = 0; i < inv.getSize(); i++) {
 
-                String name = path + ".inv.items." + i;
+                String materialName = section.getString(i + ".Material");
+                if (materialName != null) {
 
-                if (yaml.getConfig().get(name) != null) {
-                    int slot = getInteger(name + ".slot");
-                    int amount = getInteger(name + ".Amount");
-                    String type = getString(name + ".Material");
-                    ItemStack item = new ItemStack(Material.valueOf(type));
+                    Material material = Material.valueOf(materialName);
 
-                    if (yaml.getConfig().get(name + ".Meta") != null) {
-                        String display = getString(name + ".Meta.display-name");
-                        int CustomModel = getInteger(name + ".Meta.CustomModelData");
-                        ItemMeta meta = item.getItemMeta();
-                        meta.setCustomModelData(CustomModel);
-                        meta.setDisplayName(display);
+                    int slot = section.getInt(i + ".slot");
+                    int amount = section.getInt(i + ".Amount");
 
-                        item.setItemMeta(meta);
-                    }
-
-                    item.setAmount(amount);
+                    ItemStack item = new ItemStack(material, amount);
+                    int durability = section.getInt(i + ".Durability");
                     inv.setItem(slot, item);
+                }
+
+                if (materialName != null) {
+
+//
+
                 }
             }
             return inv;
         }
         return null;
+    }
+
+    public ConfigurationSection getSection() {
+        return section;
     }
 }
