c671d76f980f7b1bf8e0b966e68086673d53c9ec
YooInseo <inseo0121@gmail.com>
2022-11-08 18:28:45 +0900

    Refactor: Update Method (setValuePath)

--- src/main/java/skyexcel/data/file/util/Flowable.java
+++ src/main/java/skyexcel/data/file/util/Flowable.java
@@ -13,11 +13,17 @@ public abstract class Flowable {
 
     private JavaPlugin plugin;
 
+    private String valuePath;
+
 
     public Flowable(OfflinePlayer player, String path, JavaPlugin plugin) {
         this.player = player;
         this.config = new Config(path + "/" + player.getUniqueId());
         this.config.setPlugin(plugin);
+    }
+
+    public void setValuePath(String valuePath) {
+        this.valuePath = valuePath;
     }
 
     public boolean deposit(long amount) {
@@ -41,11 +47,11 @@ public abstract class Flowable {
 
     public void setAmount(long amount) {
 
-        config.getConfig().set("money", amount);
+        config.getConfig().set(valuePath, amount);
         config.saveConfig();
     }
 
     public long getAmount() {
-        return (config.getConfig().get("money") != null ? config.getConfig().getLong("money") : -1);
+        return (config.getConfig().get(valuePath) != null ? config.getConfig().getLong(valuePath) : -1);
     }
 }
